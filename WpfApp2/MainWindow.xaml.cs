470456637570dedfbd98552773385f63ae0160a4
Ethan <ewhensley@gmail.com>
2021-03-11 19:34:29 -0500

    im a big time hacker

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -20,9 +20,40 @@ namespace WpfApp2
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Model _myModel;
         public MainWindow()
         {
             InitializeComponent();
+            _myModel = new Model();
+            this.DataContext = _myModel;
+        }
+
+        private void Input_TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            TextBox box = (TextBox)sender;
+            if (e.Key == Key.Enter)
+            {
+                ((TextBox)sender).GetBindingExpression(TextBox.TextProperty).UpdateSource();
+            }
+        }
+
+        private void Update_TextBox_Click(object sender, RoutedEventArgs e)
+        {
+            _myModel.Update(null);
+        }
+
+        private void Operation_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Controls.Button bt = (System.Windows.Controls.Button)sender;
+            string name = bt.Name;
+
+            _myModel.Update(name);
+
+        }
+
+        private void Exit_Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
---
+++ WpfApp2/Model.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace WpfApp2
+{
+    class Model : INotifyPropertyChanged
+    {
+
+        public void Update(string op)
+        {
+            float inputLeft, inputRight, result;
+
+            float.TryParse(InputBox_Left, out inputLeft);
+            float.TryParse(InputBox_Right, out inputRight);
+
+            switch (op)
+            {
+                case "addButtn":
+                    result = inputLeft + inputRight;
+                    break;
+                case "subButtn":
+                    result = inputLeft - inputRight;
+                    break;
+                case "mulButtn":
+                    result = inputLeft * inputRight;
+                    break;
+                case "divButtn":
+                    result = inputLeft / inputRight;
+                    break;
+                default:
+                    result = 0;
+                    break;
+
+            } 
+
+            OutputBox = result.ToString();
+        }
+
+        private String _inputBox_Left;
+        public String InputBox_Left
+        {
+            get { return _inputBox_Left; }
+            set
+            {
+                _inputBox_Left = value;
+
+                OnPropertyChanged("InputBox_Left");
+            }
+        }
+
+        private String _inputBox_Right;
+        public String InputBox_Right
+        {
+            get { return _inputBox_Right; }
+            set
+            {
+                _inputBox_Right = value;
+
+                OnPropertyChanged("InputBox_Right");
+            }
+        }
+
+        private String _outputBox;
+        public String OutputBox
+        {
+            get { return _outputBox; }
+            set
+            {
+                _outputBox = value;
+
+                OnPropertyChanged("OutputBox");
+            }
+        }
+
+        #region Data Binding Stuff
+        // define our property chage event handler, part of data binding
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // implements method for data binding to any and all properties
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+
+        }
+        #endregion
+    }
+}
