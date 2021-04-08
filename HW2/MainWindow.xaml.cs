e917fd1a8fa425a9544bc9f2dd9091827b722c96
ethanhen <ewhensley@gmail.com>
2021-04-08 16:26:57 -0400

    working game, missing highlight win

--- HW2/MainWindow.xaml.cs
+++ HW2/MainWindow.xaml.cs
@@ -20,9 +20,44 @@ namespace HW2
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Model _model;
         public MainWindow()
         {
             InitializeComponent();
+
+            // make it so the user cannot resize the window
+            this.ResizeMode = ResizeMode.NoResize;
+
+            // create an instance of our Model
+            _model = new Model();
+            this.DataContext = _model;
+
+            // associate ItemControl with collection. this collection
+            // contains the tiles we placed in the ItemsControl
+            // the data in the Tile Colleciton will be bound to 
+            // each of the UI elements on the display
+            MyItemsControl.ItemsSource = _model.TileCollection;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // one of the buttons in our collection. need to figure out
+            // which one. Since we know the button is part of a collection, we 
+            // have a special way that we need to get at its bame
+
+            var selectedButton = e.OriginalSource as FrameworkElement;
+            if (selectedButton != null)
+            {
+                // get the currently selected item in the collection
+                // which we know to be a Tile object
+                // Tile has a TileName (refer to Tile.cs)
+                var currentTile = selectedButton.DataContext as Tile;
+                _model.UserSelection(currentTile.TileName);
+            }
+        }
+
+        private void Clear_Button_Click(object sender, RoutedEventArgs e)
+        {
+            _model.Clear();
         }
     }
 }
--- HW2/Tile.cs
+++ HW2/Tile.cs
@@ -4,9 +4,76 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// INotifyPropertyChanged
+using System.ComponentModel;
+
+// Brushes
+using System.Windows.Media;
+
 namespace HW2
 {
-    class Tile
+    class Tile : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private bool _tileTaken;
+        public bool TileTaken
+        {
+            get { return _tileTaken; }
+            set
+            {
+                _tileTaken = value;
+            }
+        }
+
+        private string _tileName;
+        public string TileName
+        {
+            get { return _tileName; }
+            set
+            {
+                _tileName = value;
+            }
+        }
+
+        private string _tileLabel;
+        public string TileLabel
+        {
+            get { return _tileLabel; }
+            set
+            {
+                _tileLabel = value;
+                OnPropertyChanged("TileLabel");
+            }
+        }
+
+        private Brush _tileBrush;
+        public Brush TileBrush
+        {
+            get { return _tileBrush; }
+            set
+            {
+                _tileBrush = value;
+                OnPropertyChanged("TileBrush");
+            }
+        }
+
+        private Brush _tileBackground = Brushes.Coral;
+        public Brush TileBackground
+        {
+            get { return _tileBackground; }
+            set
+            {
+                _tileBackground = value;
+                OnPropertyChanged("TileBackground");
+            }
+        }
     }
 }
