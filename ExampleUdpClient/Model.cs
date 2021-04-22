b94edf0e646ac8dbecc5eb2ec58474d51889d340
ethanhen <ewhensley@gmail.com>
2021-04-22 17:02:11 -0400

    helpful commit message that explains the changes made and provides insight for future readers

---
+++ ExampleUdpClient/Model.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Sockets
+using System.Net.Sockets;
+using System.Net;
+
+// INotifyPropertyChanged
+using System.ComponentModel;
+
+
+namespace ExampleUdpClient
+{
+    class Model : INotifyPropertyChanged
+    {
+        // some data that keeps track of ports and addresses
+        private static UInt32 _remotePort = 5000;
+        private static String _remoteIPAddress = "127.0.0.1";
+
+        // this is the UDP socket that will be used to communicate
+        // over the network
+        private UdpClient _dataSocket;
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private String _myFriendBox;
+        public String MyFriendBox
+        {
+            get { return _myFriendBox; }
+            set
+            {
+                _myFriendBox = value;
+                OnPropertyChanged("MyFriendBox");
+            }
+        }
+
+        private String _statusBox;
+        public String StatusBox
+        {
+            get { return _statusBox; }
+            set
+            {
+                _statusBox = value;
+                OnPropertyChanged("StatusBox");
+            }
+        }
+
+        private String _loopbackBox;
+        public String LoopbackBox
+        {
+            get { return _loopbackBox; }
+            set
+            {
+                _loopbackBox = value;
+                OnPropertyChanged("LoopbackBox");
+            }
+        }
+
+        public Model()
+        {
+            try
+            {
+                // set up generic UDP socket and bind to local port
+                //
+                _dataSocket = new UdpClient();
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.ToString());
+            }
+        }
+
+        public void SendMessage()
+        {
+            IPEndPoint remoteHost = new IPEndPoint(IPAddress.Parse(_remoteIPAddress), (int)_remotePort);
+            Byte[] sendBytes = Encoding.ASCII.GetBytes(MyFriendBox);
+
+            try
+            {
+                _dataSocket.Send(sendBytes, sendBytes.Length, remoteHost);
+                StatusBox += DateTime.Now + ":" + "Message Sent Successfully" + "\n";
+            }
+            catch (SocketException ex)
+            {
+                StatusBox = StatusBox + DateTime.Now + ":" + ex.ToString();
+                return;
+            }
+
+            try
+            {
+                Byte[] receiveData = _dataSocket.Receive(ref remoteHost);
+                StatusBox += DateTime.Now + ":" + "Received Loopback data" + "\n";
+                LoopbackBox += DateTime.Now + ": " + System.Text.Encoding.Default.GetString(receiveData) + "\n";
+            }
+            catch (SocketException ex)
+            {
+                StatusBox = StatusBox + DateTime.Now + ":" + ex.ToString();
+                return;
+            }
+
+            
+        }
+
+    }
+}
---
+++ ExampleUdpServer/Model.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Sockets
+using System.Net.Sockets;
+using System.Net;
+
+// Threads
+using System.Threading;
+
+// INotifyPropertyChanged
+using System.ComponentModel;
+
+
+namespace ExampleUdpServer
+{
+    class Model : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        // some data that keeps track of ports and addresses
+        private static int _localPort = 5000;
+        private static string _localIPAddress = "127.0.0.1";
+
+        // this is the thread that will run in the background
+        // waiting for incomming data
+        private Thread _receiveDataThread;
+
+        // this is the UDP socket that will be used to communicate
+        // over the network
+        UdpClient _dataSocket;
+
+
+        private String _myFriendBox;
+        public String MyFriendBox
+        {
+            get { return _myFriendBox; }
+            set
+            {
+                _myFriendBox = value;
+                OnPropertyChanged("MyFriendBox");
+            }
+        }
+
+        public Model()
+        {
+            _dataSocket = new UdpClient(_localPort);
+
+            // start the thread to listen for data from other UDP peer
+            ThreadStart threadFunction = new ThreadStart(ReceiveThreadFunction);
+            _receiveDataThread = new Thread(threadFunction);
+            _receiveDataThread.Start();
+        }
+
+        public void CleanUp()
+        {
+            // if we don't close the socket and abort the thread, 
+            // the applicatoin will not close properly
+            if (_dataSocket != null) _dataSocket.Close();
+            if (_receiveDataThread != null) _receiveDataThread.Abort();
+        }
+
+        // this is the thread that waits for incoming messages
+        private void ReceiveThreadFunction()
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(_localIPAddress), (int)_localPort);
+            while (true)
+            {
+                try
+                {
+                    Byte[] receiveData = _dataSocket.Receive(ref endPoint);
+                    MyFriendBox += DateTime.Now + ": Received - " + System.Text.Encoding.Default.GetString(receiveData) + "\n";
+                }
+                catch (SocketException ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    return;
+                }
+
+
+                
+                try
+                {
+                    Byte[] sendBytes = Encoding.ASCII.GetBytes(MyFriendBox);
+                    _dataSocket.Send(sendBytes, sendBytes.Length, endPoint);
+                    MyFriendBox += DateTime.Now + ": Sent - " + System.Text.Encoding.Default.GetString(sendBytes) + "\n";
+                }
+                catch (SocketException ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    return;
+                }
+            }
+        }
+    }
+}
